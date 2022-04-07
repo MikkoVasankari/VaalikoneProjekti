6cabfe99de821b84f04b4865615c3ed0dd45ca30
Lassi <lassi.vilkko@student.hamk.fi>
2022-04-07 12:08:45 +0300

    ulkoasun päivitystä
     ja lisätty ehdokas_num ehdokas vastauksiin

--- Vaalikone/src/main/java/data/ehdokasVastaukset.java
+++ Vaalikone/src/main/java/data/ehdokasVastaukset.java
@@ -5,12 +5,16 @@ public class ehdokasVastaukset {
 		int ehdokas_id;
 		int kysymys_id;
 		String vastaus;
+		String kommentti;
+		int ehdokas_num;
 		
 		
-		public ehdokasVastaukset(int ehdokas_id, int kysymys_id, String vastaus) {
+		public ehdokasVastaukset(int ehdokas_id, int kysymys_id, String vastaus, String kommentti, int ehdokas_num) {
 			this.ehdokas_id = ehdokas_id;
 			this.kysymys_id = kysymys_id;
 			this.vastaus = vastaus;
+			this.kommentti = kommentti;
+			this.ehdokas_num = ehdokas_num;
 		}
 		
 		public ehdokasVastaukset() {
@@ -35,5 +39,24 @@ public class ehdokasVastaukset {
 		public void setVastaus(String vastaus) {
 			this.vastaus = vastaus;
 		}
+		
+		
+
+		public String getKommentti() {
+			return kommentti;
+		}
+
+		public void setKommentti(String kommentti) {
+			this.kommentti = kommentti;
+		}
+
+		public int getEhdokas_num() {
+			return ehdokas_num;
+		}
+
+		public void setEhdokas_num(int ehdokas_num) {
+			this.ehdokas_num = ehdokas_num;
+		}
+		
 	
 }
